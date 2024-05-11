1bd07a55dc3ef7f9d5e8a6780d15fd624b408b5d
Urjitpatel28 <urjitpatel28@gmail.com>
2024-05-11 20:31:12 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace SolidConvert_Pro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBoxPDF = new System.Windows.Forms.CheckBox();
+            this.checkBoxDWG = new System.Windows.Forms.CheckBox();
+            this.lblinfoSelected = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAddfiles = new System.Windows.Forms.Button();
+            this.btnClearList = new System.Windows.Forms.Button();
+            this.btnRemoveSelected = new System.Windows.Forms.Button();
+            this.lstSelectedFiles = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSelectOutFolder = new System.Windows.Forms.Button();
+            this.txtboxOutFolder = new System.Windows.Forms.TextBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblUpdate = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtboxStatus = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // checkBoxPDF
+            // 
+            resources.ApplyResources(this.checkBoxPDF, "checkBoxPDF");
+            this.checkBoxPDF.Name = "checkBoxPDF";
+            this.checkBoxPDF.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDWG
+            // 
+            resources.ApplyResources(this.checkBoxDWG, "checkBoxDWG");
+            this.checkBoxDWG.Name = "checkBoxDWG";
+            this.checkBoxDWG.UseVisualStyleBackColor = true;
+            // 
+            // lblinfoSelected
+            // 
+            resources.ApplyResources(this.lblinfoSelected, "lblinfoSelected");
+            this.lblinfoSelected.Name = "lblinfoSelected";
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Controls.Add(this.btnAddfiles);
+            this.groupBox1.Controls.Add(this.btnClearList);
+            this.groupBox1.Controls.Add(this.btnRemoveSelected);
+            this.groupBox1.Controls.Add(this.lstSelectedFiles);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // btnAddfiles
+            // 
+            resources.ApplyResources(this.btnAddfiles, "btnAddfiles");
+            this.btnAddfiles.Name = "btnAddfiles";
+            this.btnAddfiles.UseVisualStyleBackColor = true;
+            this.btnAddfiles.Click += new System.EventHandler(this.btnAddfiles_Click);
+            // 
+            // btnClearList
+            // 
+            resources.ApplyResources(this.btnClearList, "btnClearList");
+            this.btnClearList.Name = "btnClearList";
+            this.btnClearList.UseVisualStyleBackColor = true;
+            this.btnClearList.Click += new System.EventHandler(this.btnClearList_Click);
+            // 
+            // btnRemoveSelected
+            // 
+            resources.ApplyResources(this.btnRemoveSelected, "btnRemoveSelected");
+            this.btnRemoveSelected.Name = "btnRemoveSelected";
+            this.btnRemoveSelected.UseVisualStyleBackColor = true;
+            this.btnRemoveSelected.Click += new System.EventHandler(this.btnRemoveSelected_Click);
+            // 
+            // lstSelectedFiles
+            // 
+            this.lstSelectedFiles.FormattingEnabled = true;
+            resources.ApplyResources(this.lstSelectedFiles, "lstSelectedFiles");
+            this.lstSelectedFiles.Name = "lstSelectedFiles";
+            this.lstSelectedFiles.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSelectOutFolder);
+            this.groupBox2.Controls.Add(this.txtboxOutFolder);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // btnSelectOutFolder
+            // 
+            resources.ApplyResources(this.btnSelectOutFolder, "btnSelectOutFolder");
+            this.btnSelectOutFolder.Name = "btnSelectOutFolder";
+            this.btnSelectOutFolder.UseVisualStyleBackColor = true;
+            this.btnSelectOutFolder.Click += new System.EventHandler(this.btnSelectOutFolder_Click);
+            // 
+            // txtboxOutFolder
+            // 
+            resources.ApplyResources(this.txtboxOutFolder, "txtboxOutFolder");
+            this.txtboxOutFolder.Name = "txtboxOutFolder";
+            // 
+            // btnExport
+            // 
+            resources.ApplyResources(this.btnExport, "btnExport");
+            this.btnExport.ForeColor = System.Drawing.Color.Green;
+            this.btnExport.Name = "btnExport";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // lblStatus
+            // 
+            resources.ApplyResources(this.lblStatus, "lblStatus");
+            this.lblStatus.Name = "lblStatus";
+            // 
+            // lblUpdate
+            // 
+            resources.ApplyResources(this.lblUpdate, "lblUpdate");
+            this.lblUpdate.Name = "lblUpdate";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // txtboxStatus
+            // 
+            resources.ApplyResources(this.txtboxStatus, "txtboxStatus");
+            this.txtboxStatus.Name = "txtboxStatus";
+            this.txtboxStatus.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.Controls.Add(this.txtboxStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblUpdate);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblinfoSelected);
+            this.Controls.Add(this.checkBoxDWG);
+            this.Controls.Add(this.checkBoxPDF);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBoxPDF;
+        private System.Windows.Forms.CheckBox checkBoxDWG;
+        private System.Windows.Forms.Label lblinfoSelected;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstSelectedFiles;
+        private System.Windows.Forms.Button btnClearList;
+        private System.Windows.Forms.Button btnRemoveSelected;
+        private System.Windows.Forms.Button btnAddfiles;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSelectOutFolder;
+        private System.Windows.Forms.TextBox txtboxOutFolder;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblUpdate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtboxStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace SolidConvert_Pro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int n;
+        SldWorks swApp;
+        ModelDoc2 swDoc;
+        DrawingDoc swDraw;
+        Sheet Sheet;
+        string sNewFileName;
+        string folderPath;
+        string rev;
+        ExportPdfData exportPdfData;
+
+        private void btnAddfiles_Click(object sender, EventArgs e)
+        {
+            //File selection dialogbox
+            OpenFileDialog FB = new OpenFileDialog();
+            FB.Title = "Please select files";
+            FB.Multiselect = true;
+            FB.InitialDirectory = @"Desktop";
+            FB.Filter = "SolidWorks Drawing | *slddrw";
+            FB.ShowDialog();
+
+            //Add selected file to list
+            string[] sFilenames = FB.FileNames;
+            n = sFilenames.Length;
+
+            string sFilename;
+
+            for (int i = 0; i < n; i = i + 1)
+            {
+                sFilename = sFilenames[i];
+                lstSelectedFiles.Items.Add(sFilename);
+            }
+
+            n = lstSelectedFiles.Items.Count;
+            lblinfoSelected.Text = n + " files selected.";
+            txtboxStatus.Text = $"0 / {n}";
+        }
+
+        private void btnRemoveSelected_Click(object sender, EventArgs e)
+        {
+            //remove selected files from list
+            var selectedItems = lstSelectedFiles.SelectedItems.Cast<object>().ToList();
+
+            foreach (var selectedItem in selectedItems)
+            {
+                lstSelectedFiles.Items.Remove(selectedItem);
+            }
+
+            n = lstSelectedFiles.Items.Count;
+            lblinfoSelected.Text = n + " files selected.";
+            txtboxStatus.Text = $"0 / {n}";
+        }
+
+        private void btnClearList_Click(object sender, EventArgs e)
+        {
+            //clear list
+            lstSelectedFiles.Items.Clear();
+            n = lstSelectedFiles.Items.Count;
+            lblinfoSelected.Text = n + " files selected.";
+            txtboxStatus.Text = $"0 / {n}";
+            lblStatus.Text = string.Empty;
+            lblUpdate.Text = string.Empty;
+        }
+
+        private void btnSelectOutFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.Description = "Please select the desired folder";
+            // Show the FolderBrowserDialog.
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+                txtboxOutFolder.Text = folderPath;
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            //Path validation
+            if (txtboxOutFolder.Text.Length == 0)
+            {
+                lblStatus.Text = "Please select output folder.";
+                lblStatus.ForeColor = Color.Red;
+            }
+
+            if (txtboxOutFolder.Text.Length > 0)
+            {
+                folderPath = txtboxOutFolder.Text;
+
+                if (IsValidFolderPath(folderPath))
+                {
+                    if (lstSelectedFiles.Items.Count == 0)
+                    {
+                        lblStatus.Text = "Please select file to convert.";
+                        lblStatus.ForeColor = Color.Red;
+                    }
+                    else
+                    {
+                        txtboxOutFolder.Text = folderPath;
+                        if (checkBoxPDF.Checked || checkBoxDWG.Checked)
+                        {
+                            lblStatus.Text = "Starting Conversation...";
+                            lblStatus.ForeColor = Color.Blue;
+
+                            ConnectToSolidworks();
+
+                            int i = 1;
+                            foreach (var item in lstSelectedFiles.Items)
+                            {
+
+                                txtboxStatus.Text = $"{i} / {n}";
+                                lblStatus.Text = "Converting...";
+                                lblStatus.ForeColor = Color.Blue;
+                                lblUpdate.Text = System.IO.Path.GetFileNameWithoutExtension(item.ToString());
+                                lblUpdate.ForeColor = Color.BlueViolet;
+
+                                OpenDrawing(Convert.ToString(item));
+
+                                if (checkBoxPDF.Checked)
+                                {
+                                    PDF(Convert.ToString(item));
+                                }
+                                if (checkBoxDWG.Checked)
+                                {
+                                    DWG(Convert.ToString(item));
+                                }
+                                CloseDrawing(Convert.ToString(item));
+
+                                i++;
+                            }
+
+                            lblStatus.Text = "Completed";
+                            lblStatus.ForeColor = Color.Green;
+                            lblUpdate.Text = string.Empty;
+                        }
+                        else
+                        {
+                            lblStatus.Text = "Please select export file type.";
+                            lblStatus.ForeColor = Color.Red;
+                        }
+                    }
+                }
+                else
+                {
+                    lblStatus.Text = "The folder path is not valid.";
+                    lblStatus.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        static bool IsValidFolderPath(string folderPath)
+        {
+            bool isValid = false;
+
+            try
+            {
+                // Check if the folder path exists
+                if (Directory.Exists(folderPath))
+                {
+                    isValid = true;
+                }
+            }
+            catch (System.Exception)
+            {
+                // Handle exception if needed
+            }
+            return isValid;
+        }
+
+        private void ConnectToSolidworks()
+        {
+            swApp = null;
+
+            try
+            {
+                // Try to get an active instance of SolidWorks
+                swApp = (SldWorks)Marshal.GetActiveObject("sldworks.Application");
+            }
+            catch (Exception)
+            {
+                // SolidWorks is not running, create a new instance
+                lblStatus.Text = "Opening SolidWorks...";
+                lblStatus.ForeColor = Color.Blue;
+                swApp = new SldWorks();
+                swApp.Visible = true;
+            }
+
+            //set solidworks
+            swApp.SetUserPreferenceIntegerValue((int)swUserPreferenceIntegerValue_e.swDxfMultiSheetOption, (int)swDxfMultisheet_e.swDxfActiveSheetOnly);
+            swApp.SetUserPreferenceIntegerValue((int)swUserPreferenceIntegerValue_e.swDxfVersion, (int)swDxfFormat_e.swDxfFormat_R2000);
+            swApp.SetUserPreferenceIntegerValue((int)swUserPreferenceIntegerValue_e.swDxfOutputFonts, 1);
+            swApp.SetUserPreferenceIntegerValue((int)swUserPreferenceIntegerValue_e.swDxfOutputLineStyles, 1);
+        }
+
+        private void OpenDrawing(String Filename)
+        {
+            swDoc = null;
+            swDoc = swApp.OpenDoc6(Filename, (int)swDocumentTypes_e.swDocDRAWING, (int)swOpenDocOptions_e.swOpenDocOptions_Silent, string.Empty, 0, 0);
+
+            //get revision
+            CustomPropertyManager swCustProp = swDoc.Extension.CustomPropertyManager[""];
+            rev = swCustProp.Get("Revision");
+
+            //pdf view after saving elimination
+            exportPdfData = swApp.GetExportFileData(1);
+            exportPdfData.ViewPdfAfterSaving = false;
+        }
+
+        private void PDF(String Filename)
+        {
+            sNewFileName = folderPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(Filename) + "_" + rev + ".pdf";
+            swDoc.Extension.SaveAs(sNewFileName,0,0, exportPdfData,0,0);
+        }
+        private void DWG(String Filename)
+        {
+            swDraw = (DrawingDoc)swDoc;
+            if (swDraw.GetSheetCount() == 1)
+            {
+                sNewFileName = folderPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(Filename) + "_" + rev + ".dwg";
+                swDoc.SaveAs(sNewFileName);
+            }
+
+            if (swDraw.GetSheetCount() > 1)
+            {
+                int i = 1;
+                string[] vSheet = (string[])swDraw.GetSheetNames();
+                foreach (string v in vSheet)
+                {
+                    swDraw.ActivateSheet(v);
+                    sNewFileName = folderPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(Filename) + "_" + rev + "-" + i + ".dwg";
+                    swDoc.SaveAs(sNewFileName);
+                    i++;
+                }
+            }
+        }
+
+        private void CloseDrawing(String Filename)
+        {
+            //swApp.CloseDoc(Filename);
+            swApp.QuitDoc(Filename);
+        }
+    }
+}
